844ea73884ce4d39144d6d0b52ccb86fe404fe16
tl-lisa <tl-lisa@truelovelive.dev>
2020-03-09 14:52:13 +0800

    for gift-v2

--- identity/test_getGiftCategory.py
+++ identity/test_getGiftCategory.py
@@ -1,5 +1,5 @@
 #/api/v2/identity/giftCategory/list?type=
-#mileston 18 加入軟刪及判斷delete_at
+#mileston 18 加入軟刪及判斷delete_at,並且使用V2 table
 import json
 import requests
 import pymysql
@@ -10,33 +10,59 @@ from ..assistence import api
 from ..assistence import initdata
 from ..assistence import dbConnect
 from pprint import pprint
+from datetime import datetime, timedelta
 
 env = 'testing'
 test_parameter = {}
 header = {'Content-Type': 'application/json', 'Connection': 'Keep-alive', 'X-Auth-Token': '', 'X-Auth-Nonce': ''}
 
-
 initdata.set_test_data(env, test_parameter)
-
 
 class TestGetGiftCategory():
     gctype = ['live_room', 'liveshow', 'post_gift', 'instant_message']
-    
-    def setup_class(self):
-        sqlList = ["update gift_category set end_time = NULL, start_time = NULL where status = 1"]
+
+    def setData(self, startTime, endTime, isDelete):
+        sqlList = []
+        if startTime == 'Null':
+            startTimeStr = 'start_time = NULL'
+        elif startTime == 'small':
+            startTimeStr = "start_time = '" + (datetime.today() - timedelta(days=2)).strftime('%Y-%m-%d  %H:%M:%S') + "'"
+        elif startTime == 'bigger':
+            startTimeStr = "start_time = '" + (datetime.today() + timedelta(days=2)).strftime('%Y-%m-%d  %H:%M:%S') + "'"
+
+        if endTime == 'Null':
+            endTimeStr = 'end_time = NULL'
+        elif endTime == 'small':
+            endTimeStr = "end_time = " + (datetime.today() - timedelta(days=2)).strftime('%Y-%m-%d  %H:%M:%S') + "'"
+        elif endTime == 'bigger':
+            endTimeStr = "end_time = " + (datetime.today() + timedelta(days=2)).strftime('%Y-%m-%d  %H:%M:%S') + "'"
+
+        sqlStr = "update gift_category_v2 set " + startTimeStr + " , " +  endTimeStr
+        if isDelete:
+            sqlStr += ", delelet_at = '" + (datetime.today() - timedelta(days=3)).strftime('%Y-%m-%d  %H:%M:%S') + "'"
+        print(sqlStr)
+        sqlList.append(sqlStr)
         dbConnect.dbSetting(test_parameter['db'], sqlList)
-    
-    @pytest.mark.parametrize("test_input, expected", [
-        ([test_parameter['backend_token'], test_parameter['backend_nonce'], 1], 2),
-        ([test_parameter['user_token'], test_parameter['user_nonce'], 2], 2),
-        ([test_parameter['broadcaster_token'], test_parameter['broadcaster_nonce'], 3], 2),
-        ([test_parameter['user_token'], test_parameter['user_nonce'], 4], 2),
-        ([test_parameter['user_token'], test_parameter['user_nonce'], 5], 5),
-        ([test_parameter['err_token'], test_parameter['err_nonce'], 1], 4)
+
+    @pytest.mark.parametrize("test_input, expected",[ 
+        ([test_parameter['backend_token'], test_parameter['backend_nonce'], 1], 'Null', 'Null', False, 2),
+        ([test_parameter['user_token'], test_parameter['user_nonce'], 2], 'Null', 'equal', False, 2),
+        ([test_parameter['broadcaster_token'], test_parameter['broadcaster_nonce'], 3], 'Null', 'small', False, 2),
+        ([test_parameter['user_token'], test_parameter['user_nonce'], 4], 'Null', 'bigger', False, 2),
+        ([test_parameter['user_token'], test_parameter['user_nonce'], 4], 'small', 'Null', False, 2),
+        ([test_parameter['user_token'], test_parameter['user_nonce'], 4], 'bigger', 'Null', False, 2),
+        ([test_parameter['user_token'], test_parameter['user_nonce'], 4], 'small', 'small', False, 2),
+        ([test_parameter['user_token'], test_parameter['user_nonce'], 4], 'bigger', 'bigger', False, 2),
+        ([test_parameter['user_token'], test_parameter['user_nonce'], 4], 'small', 'bigger', False, 2),
+        ([test_parameter['user_token'], test_parameter['user_nonce'], 4], 'small', 'bigger', False, 2),
+        ([test_parameter['user_token'], test_parameter['user_nonce'], 5], 'small', 'bigger', False, 4),
+        ([test_parameter['err_token'], test_parameter['err_nonce'], 1], 'small', 'bigger', False,4)
     ])
-    def testAuthAndType(self, test_input, expected):
+    def testAuthAndType(self, test_input, startTime, endTime, isDelete, expected):
+        TimeCondition1 = (datetime.today() - timedelta(minutes=1)).strftime('%Y-%m-%d  %H:%M:%S')
+        TimeCondition2 = (datetime.today() + timedelta(minutes=1)).strftime('%Y-%m-%d  %H:%M:%S')
         #驗證權限及查詢類別
-        now = int(time.time()) * 1000
+        self.setData(startTime, endTime, isDelete)
         apiName = '/api/v2/identity/giftCategory/list?type=' + str(test_input[2]) + '&item=20&page=1'
         header['X-Auth-Token'] = test_input[0]
         header['X-Auth-Nonce'] = test_input[1]
@@ -46,8 +72,8 @@ class TestGetGiftCategory():
         assert res.status_code // 100 == expected
         if expected == 2:
             if  test_input[2] < 5:
-                sql = "select id, category_name, banner, url from gift_category where type = '" + self.gctype[test_input[2] - 1] + "' and  status = 1 and "
-                sql += "((start_time <= " + str(now) + " or start_time is null) and (end_time >= " + str(now) + " or end_time is null))order by id desc"
+                sql = "select id, category_name, banner, url from gift_category_v2 where type = '" + self.gctype[test_input[2] - 1] + "' and  deleted_at is null and "
+                sql += "((start_time <= '" + TimeCondition1 + "' or start_time is null) and (end_time >= '" + TimeCondition2 + "' or end_time is null)) order by id desc"
                 dbResult = dbConnect.dbQuery(test_parameter['db'], sql)
                 #pprint(dbResult)
                 assert restext['totalCount'] == len(dbResult)
@@ -57,26 +83,6 @@ class TestGetGiftCategory():
                         assert restext['data'][i]['type'] == test_input[2]
                         assert restext['data'][i]['categoryName'] == dbResult[i][1]
                         assert restext['data'][i]['banner'] in ('', dbResult[i][2])
-                        assert restext['data'][i]['url'] in ('', dbResult[i][2])
+                        assert restext['data'][i]['url'] in ('', dbResult[i][3])
             else:
                 assert restext['totalCount'] == 0
-
-    @pytest.mark.parametrize("start_time, end_time, expected", [
-        (str(int(time.time() + 180) * 1000), str(int(time.time() + 300) * 1000), 0),
-        (str(int(time.time() + 61) * 1000), str(int(time.time() + 180) * 1000), 1),
-        (str(int(time.time() - 180) * 1000), str(int(time.time() - 120) * 1000), 0),
-        (str(int(time.time() - 180) * 1000), str(int(time.time() + 62) * 1000), 1)
-        ])
-    def testExpiryTime(self, start_time, end_time, expected):
-        #禮物類別的start time大於目前時間，不應查出；需加判cache 時間60秒
-        time.sleep(60)
-        sqlList = ["update gift_category set end_time = " + end_time + ", start_time = " + start_time + " where type = 'post_gift'"]
-        dbConnect.dbSetting(test_parameter['db'], sqlList)
-        apiName = '/api/v2/identity/giftCategory/list?type=3&item=20&page=1'
-        header['X-Auth-Token'] = test_parameter['backend_token']
-        header['X-Auth-Nonce'] = test_parameter['backend_nonce']
-        res = api.apiFunction(test_parameter['prefix'], header, apiName, 'get', None)  
-        restext = json.loads(res.text)  
-        assert res.status_code // 100 == 2
-        assert restext['totalCount'] == expected
-
